fdb1da67849e01864527fbe1de461f408f8e1de9
efi1 <efovadia@gmail.com>
2022-10-16 15:41:57 +0300

    failing a test for testing jenkins behaviour.

--- tests/test_github_activities.py
+++ tests/test_github_activities.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pytest
+
 logging.getLogger()
 
 
@@ -16,12 +17,14 @@ def test_fork_repo(tests_client, tests_data):
 
 def test_clone(tests_client, tests_data):
     forked_repo = tests_client.get_forked_repo()
-    assert os.path.exists(tests_data.git_tests_resource) == False, F"{tests_data.git_tests_resource} directory is not empty"
+    assert os.path.exists(
+        tests_data.git_tests_resource) == False, F"{tests_data.git_tests_resource} directory is not empty"
     cloned_repo = tests_client.get_cloned_repo(forked_repo)
     working_dir = cloned_repo.working_dir
     # check that cloned was a successful
     assert working_dir == os.path.join(tests_data.git_tests_resource, tests_data.repo_name), 'clone failed'
     assert os.path.exists(os.path.join(tests_data.git_tests_resource, tests_data.repo_name)) is True, 'clone failed'
+    assert True == False, 'failed due to jenkins testing'
 
 
 def test_branch_creation(tests_client, tests_data):
@@ -32,7 +35,7 @@ def test_branch_creation(tests_client, tests_data):
     branches_after = set(cloned_repo.branches)
     assert branches_after.difference(
         branches_before).pop().name == tests_data.branch_name, F"branch {tests_data.branch_name} missing"
-    new_branch.checkout() # move to new branch
+    new_branch.checkout()  # move to new branch
     assert cloned_repo.active_branch == new_branch
 
 
@@ -40,7 +43,7 @@ def test_create_new_file(tests_client, tests_data):
     forked_repo = tests_client.get_forked_repo()
     cloned_repo = tests_client.get_cloned_repo(forked_repo)
     new_branch = cloned_repo.create_head(tests_data.branch_name)
-    new_branch.checkout() # move to new branch
+    new_branch.checkout()  # move to new branch
     working_dir = cloned_repo.working_dir
     with open(F"{working_dir}{os.sep}{tests_data.file_name}", 'w') as file:
         file.write(tests_data.file_text)
@@ -52,7 +55,7 @@ def test_edit_readme(tests_client, tests_data):
     forked_repo = tests_client.get_forked_repo()
     cloned_repo = tests_client.get_cloned_repo(forked_repo)
     new_branch = cloned_repo.create_head(tests_data.branch_name)
-    new_branch.checkout() # move to new branch
+    new_branch.checkout()  # move to new branch
     working_dir = cloned_repo.working_dir
     with open(F"{working_dir}{os.sep}README.txt", 'a') as file:
         file.write(tests_data.readme_text)
@@ -68,7 +71,7 @@ def test_commit_changes(github_client, tests_client, tests_data):
     forked_repo = tests_client.get_forked_repo()
     cloned_repo = tests_client.get_cloned_repo(forked_repo)
     new_branch = cloned_repo.create_head(tests_data.branch_name)
-    new_branch.checkout() # move to new branch
+    new_branch.checkout()  # move to new branch
     working_dir = cloned_repo.working_dir
     with open(F"{working_dir}{os.sep}{tests_data.file_name}", 'w') as file:
         file.write(tests_data.file_text)
@@ -77,7 +80,7 @@ def test_commit_changes(github_client, tests_client, tests_data):
     with open(F"{working_dir}{os.sep}README.txt", 'a') as file:
         file.write(tests_data.readme_text)
     assert tests_client.get_changes(cloned_repo) == ['README.txt'], 'no git tracking for modify in README.txt file'
-    cloned_repo.git.add(all=True) # adding all changes to stage
+    cloned_repo.git.add(all=True)  # adding all changes to stage
     stage_after_add = tests_client.get_stage(cloned_repo)  # check files in stage
     assert len(set(stage_after_add).difference({'README.txt', tests_data.file_name})) == 0, 'not all changes in stage'
     cloned_repo.index.commit(F"add a new file: {''.join(untracked_files)}, add text to README.txt file")
@@ -88,13 +91,13 @@ def test_push_and_verify(tests_client, tests_data):
     forked_repo = tests_client.get_forked_repo()
     cloned_repo = tests_client.get_cloned_repo(forked_repo)
     new_branch = cloned_repo.create_head(tests_data.branch_name)
-    new_branch.checkout() # move to new branch
+    new_branch.checkout()  # move to new branch
     working_dir = cloned_repo.working_dir
     with open(F"{working_dir}{os.sep}{tests_data.file_name}", 'w') as file:
         file.write(tests_data.file_text)
     with open(F"{working_dir}{os.sep}README.txt", 'a') as file:
         file.write(tests_data.readme_text)
-    cloned_repo.git.add(all=True) # adding all changes to stage
+    cloned_repo.git.add(all=True)  # adding all changes to stage
     cloned_repo.index.commit(F"creation of {tests_data.file_name} and text added to README.txt file")
     origin = cloned_repo.remote(name='origin')
     origin.push(refspec=F"{tests_data.branch_name}")
@@ -102,4 +105,3 @@ def test_push_and_verify(tests_client, tests_data):
     # if no difference, it implies that all changes were pushed successfully
     assert not cloned_repo.git.diff(tests_data.branch_name,
                                     tests_data.remote_branch) is False, 'remote branch is differ than the local one'
-
